204d7ebd4d487c532a4b3e3d24afdc34806019f4
Juyoung <juyoung7@illinois.edu>
2021-03-22 00:55:16 +0900

    Made text inputs and a login button

--- login-design/src/App.js
+++ login-design/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = () => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className='Login'>
+        <h1>Enter your username and password to login</h1>
+          <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username'/>
+          <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password'/>
+          <button type="button" onClick={login}>Login</button>
+      </div>
+  )
 }
 
 export default App;

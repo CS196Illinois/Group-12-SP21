ee3cf71f586f2ca48208cde2a364392a94b8912d
Juyoung <juyoung7@illinois.edu>
2021-03-26 21:43:17 +0900

    Added some design

--- login/src/App.js
+++ login/src/App.js
@@ -14,16 +14,29 @@ const App = () => {
 	
 
 	return (
-	    <div className='Login'>
-	      <h1>Enter your username and password to login</h1>
-	        <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username'/>
-	        <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password'/>
-	        <button type="button" onClick={login}>Login</button>
+	    <div class="container" className='login'>
+        <form onSubmit={login}>
+	        <h1>Spotify Data Visualization</h1>
+
+          <br></br>
+
+          <label id='label' for='username'><b>Username</b></label>
+	        <input id='box' type='text' value={username} onChange={(e) => setUsername(e.target.value)}
+           placeholder='Enter Username' name='username' required/>
+	        
+          <label for='username'><b>Password</b></label>
+          <input id='box' type='text' value={password} onChange={(e) => setPassword(e.target.value)}
+           placeholder='Enter Password' name='password' required/>
+	        
+          <button type='submit'>Sign In</button>
+
+          <label id='label' for='remember'><b>Remember Me</b></label>
+          <input type='checkbox' name='remember'/>
+          <footer></footer>
+        </form>
 	    </div>
 	)
 }
 	
 
 export default App;
-
-

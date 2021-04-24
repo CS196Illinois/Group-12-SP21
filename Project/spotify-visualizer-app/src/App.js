3540a83818d698f516e6c2cf7bdae4ff709cd441
Juyoung <juyoung7@illinois.edu>
2021-04-24 13:21:11 +0900

    new mock bar chart

--- Project/spotify-visualizer-app/src/App.js
+++ Project/spotify-visualizer-app/src/App.js
@@ -1,6 +1,55 @@
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Bar } from 'react-chartjs-2';
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '#',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    }
+  ]
+}
+
+const options = {
+  indexAxis: 'y',
+  // Elements options apply to all of the options unless overridden in a dataset
+  // In this case, we are setting the border of each horizontal bar to be 2px wide
+  elements: {
+    bar: {
+      borderWidth: 2,
+    },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'right',
+    },
+    title: {
+      display: true,
+      text: 'Sample bar chart',
+    },
+  },
+};
 
 const App = () => {  
 	const [username, setUsername] = useState('');
@@ -9,6 +58,9 @@ const App = () => {
     <div>
       <h1>Hi {username}!</h1>
       <button onClick={() => history.push('/')}>Go to home</button>
+      <div>
+        <Bar data={data} options={options}/>
+      </div>
     </div>
   );
 
@@ -26,9 +78,7 @@ const App = () => {
                 <label id='label' for='username'><b>Username</b></label>
                 <input id='box' type='text' value={username} onChange={(e) => setUsername(e.target.value)}
                   placeholder='Enter Username' name='username' required/>
-
         
-          
                 <nav>
                   <a href={`/user/${username}`}><button id='button'>Sign In</button></a>
                 </nav>

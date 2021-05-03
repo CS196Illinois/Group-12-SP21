cc6a884678334564786e417a0b14c710fb14f351
Juyoung <juyoung7@illinois.edu>
2021-05-04 01:59:09 +0900

    design changes to visualization page

--- Project/spotify-visualizer-app/src/App.js
+++ Project/spotify-visualizer-app/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Bar, Pie } from 'react-chartjs-2';
 
-//data for barchart
-const bardata = {
+//data for horizontal bar chart #1
+const bardata1 = {
   labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
   datasets: [
     {
@@ -31,8 +31,8 @@ const bardata = {
   ]
 }
 
-//options for barchart
-const baroptions = {
+//options for horizontal bar chart #1
+const baroptions1 = {
   indexAxis: 'y',
   // Elements options apply to all of the options unless overridden in a dataset
   // In this case, we are setting the border of each horizontal bar to be 2px wide
@@ -42,6 +42,7 @@ const baroptions = {
     },
   },
   responsive: true,
+  maintainAspectRatio: false,
   plugins: {
     legend: {
       position: 'right',
@@ -53,8 +54,8 @@ const baroptions = {
   },
 };
 
-//data for piechart
-const piedata= {
+//data for pie chart #1
+const piedata1= {
   labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
   datasets: [
     {
@@ -81,16 +82,141 @@ const piedata= {
   ],
 };
 
+//options for pie chart #1
+const pieoptions1 = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: 'right',
+    },
+    title: {
+      display: true,
+      text: 'Sample pie chart',
+    },
+  },
+};
+
+//data for horizontal bar chart #2
+const bardata2 = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '#',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    }
+  ]
+}
+
+//options for horizontal bar chart #2
+const baroptions2 = {
+  indexAxis: 'y',
+  // Elements options apply to all of the options unless overridden in a dataset
+  // In this case, we are setting the border of each horizontal bar to be 2px wide
+  elements: {
+    bar: {
+      borderWidth: 2,
+    },
+  },
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: 'right',
+    },
+    title: {
+      display: true,
+      text: 'Sample bar chart',
+    },
+  },
+};
+
+//data for pie chart #2
+const piedata2= {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+//options for pie chart #2
+const pieoptions2 = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: 'right',
+    },
+    title: {
+      display: true,
+      text: 'Sample pie chart',
+    },
+  },
+};
+
 const App = () => {  
 
   //visualization page
   const Visualization = ({history}) => (
     <div>
-      <h1>Hi!</h1>
-      <button onClick={() => history.push('/')}>Go to home</button>
-      <div>
-        <Bar data={bardata} options={baroptions}/>
-        <Pie data={piedata}/>
+      <h1>This is a sample data visualization page</h1>
+
+      <br></br>
+
+      <button className="go-home" onClick={() => history.push('/')}>Home</button>
+      <a href="https://github.com/CS196Illinois/Group-12-SP21">
+        <button className="github">GitHub</button>
+      </a>
+      <div className="chart-container">
+        <article className="horizontalbar-container">
+          <Bar data={bardata1} options={baroptions1}/>
+        </article>
+        <article className="pie-container">
+          <Pie data={piedata1} options={pieoptions1}/>
+        </article>
+        <article className="horizontalbar-container">
+          <Bar data={bardata2} options={baroptions2}/>
+        </article>
+        <article className="pie-container">
+          <Pie data={piedata2} options={pieoptions2}/>
+        </article>
       </div>
     </div>
   );
@@ -103,7 +229,7 @@ const App = () => {
           <Switch>
             <Route path="/" exact render={() =>
               <div className='container'>
-                <h1>Spotify Data Visualization</h1>
+                <h1>Spotify Data Visualizer</h1>
 
                 <br></br>
 
